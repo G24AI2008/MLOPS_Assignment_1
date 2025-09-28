8d0a968cd2e499961932d96287f1bf45dc916d71
G24AI2008 <g24ai2008@iitj.ac.in>
2025-09-28 10:01:47 +0530

    Adding train2.py (KernelRidge) training script

---
+++ train2.py
@@ -0,0 +1,6 @@
+from misc import run_training_pipeline
+from sklearn.kernel_ridge import KernelRidge
+
+if __name__ == "__main__":
+	print("__Running KernelRidge Training Pipeline__")
+	run_training_pipeline(KernelRidge)
